6c33e2c5e6935afa31dcf84e55e47300d3d5a206
Luke D <lukedary@gmail.com>
2015-03-18 14:22:08 -0400

    Add Simple and Complex Main
    Remove executables and update to the flagged version of main for port.

--- simple.go
+++ simple.go
@@ -2,12 +2,10 @@ package main
 
 import (
     "net/http"
-)
+    )
 
 func main() {
     fs := http.FileServer(http.Dir("."))
     http.Handle("/", fs)
-    
-    //log.Println("Listening...")
     http.ListenAndServe(":8080", nil)
 }
